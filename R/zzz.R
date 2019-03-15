c504233297824d42ae00d0fd413365bd212fbc12
Yi Pan <Y.Pan@bham.ac.uk>
2019-03-15 09:11:28 +0000

    first commit

---
+++ R/zzz.R
@@ -0,0 +1,14 @@
+#' lapacker
+#'
+#' Provide the 'lapacke.h' header file for R. 'LAPACKE' is a
+#' collection of wrappers that provide a C interface to the FORTRAN 'LAPACK'
+#' library. Currently only the double precision / double complex 'LAPACK'
+#' routines are supported.
+#'
+#' @docType package
+#' @author Yi Pan
+#' @import Rcpp
+#' @importFrom Rcpp evalCpp
+#' @useDynLib lapacker
+#' @name lapacker
+NULL
